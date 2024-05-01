c2b571eaed520cb60b1a1678d94f59bb72e35018
RajkumarMondal66 <148803019+RajkumarMondal66@users.noreply.github.com>
2024-05-01 17:13:30 +0530

    Update petrophysics.py

--- petrophysics.py
+++ petrophysics.py
@@ -8,9 +8,9 @@ import io
 import plotly.express as px
 import plotly.graph_objects as go
 
-st.title("welcome ladies, Gentelman and Others")
-st.header("Petrophysical Measurement analysis and visualisation")
-st.subheader("Using python")
+st.title("Welcome ladies, Gentleman and Others")
+st.header("Petrophysical Measurement Analysis and Visualisation")
+st.subheader("Using Python")
 #text=st.button("click")
 st.title("NOTE : ")
 st.markdown("please see your log short notation and then change in Las log Name Changer section after Data Statistics as following - DEPTH(depth), CALI(Caliper),GR(gamma ray),NPHI(porosity),RHOB(density),RESD(deep resistivity Rt),RESS(shallow resistivity Rx0)")
